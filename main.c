f302775b69c9b0dc11f470bb16cc942923356336
Josh <jtdonahue360@gmail.com>
2019-02-17 19:31:33 -0500

    switch to unity build

--- main.c
+++ main.c
@@ -15,47 +15,10 @@
 #include <string.h>
 #include <stdarg.h>
 
-
-#define MAX(x, y) ((x) > (y) ? (x) : (y))
-#define MIN(x, y) ((x) < (y) ? (x) : (y))
-
-//stretchy buffers
+#include "common.c"
 
 
-typedef struct BufHdr {
-	size_t len;
-	size_t cap;
-	char buf[0];
-} BufHdr;
 
-void* buf__grow(const void*, size_t, size_t);
-
-#define buf__hdr(b) ((BufHdr *)((char *)b - offsetof(BufHdr, buf)))
-#define buf__fits(b, n) (buf_len(b) + (n) <= buf_cap(b))
-#define buf__fit(b, n) (buf__fits(b, n) ? 0 : ((b) = buf__grow((b), buf_len(b) + n, sizeof(*(b)))))
-
-#define buf_len(b) ((b) ? buf__hdr(b)->len : 0)
-#define buf_cap(b) ((b) ? buf__hdr(b)->cap : 0)
-#define buf_end(b) ((b) + buf_len(b))	
-//#define buf_push(b, x) (buf__fit(b, 1), b[buf_len(b)] = (x), buf__hdr(b)->len++)
-#define buf_push(b, ...) (buf__fit((b), 1), (b)[buf__hdr(b)->len++] = (__VA_ARGS__))
-#define buf_free(b) ((b) ? free(buf__hdr(b)) : 0)
-
-
-void *buf__grow(const void * buf, size_t new_len, size_t elem_size) {
-	size_t new_cap = MAX(1 + 2 * buf_cap(buf), new_len);
-	assert(new_len <= new_cap);
-	size_t new_size = offsetof(BufHdr, buf) + new_cap * elem_size;
-	BufHdr *new_hdr;
-	if (buf) {
-		new_hdr = realloc(buf__hdr(buf), new_size);
-	} else {
-		new_hdr = malloc(new_size);
-		new_hdr->len = 0;
-	}
-	new_hdr->cap = new_cap;
-	return new_hdr->buf;
-}
 
 
 
